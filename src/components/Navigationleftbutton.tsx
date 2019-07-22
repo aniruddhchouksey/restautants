646f4e32041de82a8e51a1516e99e313dc58316e
Rohit sahu <rohitsahu.4998@gmail.com>
2019-07-22 18:24:09 +0530

    check

--- src/components/Navigationleftbutton.tsx
+++ src/components/Navigationleftbutton.tsx
@@ -1,28 +1,35 @@
-import React, {Dispatch} from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, {Dispatch, useEffect} from "react";
+import {makeStyles} from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/ChevronLeft";
 
 const useStyles = makeStyles(theme => ({
-  margin: {
-    margin: theme.spacing(1)
-  }
+    margin: {
+        margin: theme.spacing(1)
+    }
 }));
 
-export default function ButtonSizes(props:{date:Date,onClick: (date:Date)=>void }) {
-  const classes = useStyles();
+export default function ButtonSizes(props: { date: Date, onClick: (date: Date) => void }) {
+    const classes = useStyles();
+    let disabled=false;
+    let nextDate = new Date(props.date);
+    nextDate.setDate(nextDate.getDate() - 7);
+    console.log(new Date(),nextDate)
+    let startofWeek =new Date()
+   startofWeek.setDate(new Date().getDate() - new Date().getDay());
 
-    function decrementWeek(){
-        let nextDate = new Date(props.date);
-        nextDate.setDate(nextDate.getDate()-7);
-        if(new Date().getSeconds()< nextDate.getSeconds())
-        props.onClick(nextDate)
+    if ( startofWeek  > nextDate)
+        disabled=true;
+    function decrementWeek() {
+            props.onClick(nextDate)
     }
-  return (
-    <div>
-      <Fab color="secondary" onClick={decrementWeek} aria-label="Add" className={classes.margin}>
-        <AddIcon fontSize="large" />
-      </Fab>
-    </div>
-  );
+
+    return (
+        <div>
+            <Fab color="secondary" onClick={decrementWeek} aria-label="Add" disabled={disabled}
+                 className={classes.margin}>
+                <AddIcon fontSize="large"/>
+            </Fab>
+        </div>
+    );
 }
